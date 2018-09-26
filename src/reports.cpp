7542688b1092b3bc8e2c9f9720963c712f4bd6bf
Thomas Kemmer <thomas@binaryden.de>
2018-09-26 17:40:36 +0200

    Enable exp/obs output for all types

--- src/reports.cpp
+++ src/reports.cpp
@@ -20,8 +20,48 @@ void badhron_print_report(badhron::CheckReport::Prototype* report) {
 	switch(report->ctype) {
 		case badhron::CheckType::Bool:
 			std::cout << std::boolalpha;
-			std::cout << "Expected:\t" << reinterpret_cast<bool*>(report->expected)[0] << '\n';
-			std::cout << "Observed:\t" << reinterpret_cast<bool*>(report->observed)[0] << '\n';
+			std::cout << "Expected:\t" << *reinterpret_cast<bool*>(report->expected) << '\n';
+			std::cout << "Observed:\t" << *reinterpret_cast<bool*>(report->observed) << '\n';
+			break;
+		case badhron::CheckType::Int64:
+			std::cout << "Expected:\t" << *reinterpret_cast<int64_t*>(report->expected) << '\n';
+			std::cout << "Observed:\t" << *reinterpret_cast<int64_t*>(report->observed) << '\n';
+			break;
+		case badhron::CheckType::Int32:
+			std::cout << "Expected:\t" << *reinterpret_cast<int32_t*>(report->expected) << '\n';
+			std::cout << "Observed:\t" << *reinterpret_cast<int32_t*>(report->observed) << '\n';
+			break;
+		case badhron::CheckType::Int16:
+			std::cout << "Expected:\t" << *reinterpret_cast<int16_t*>(report->expected) << '\n';
+			std::cout << "Observed:\t" << *reinterpret_cast<int16_t*>(report->observed) << '\n';
+			break;
+		case badhron::CheckType::Int8:
+			std::cout << "Expected:\t" << int(*report->expected) << '\n';
+			std::cout << "Observed:\t" << int(*report->observed) << '\n';
+			break;
+		case badhron::CheckType::UInt64:
+			std::cout << "Expected:\t" << *reinterpret_cast<uint64_t*>(report->expected) << '\n';
+			std::cout << "Observed:\t" << *reinterpret_cast<uint64_t*>(report->observed) << '\n';
+			break;
+		case badhron::CheckType::UInt32:
+			std::cout << "Expected:\t" << *reinterpret_cast<uint32_t*>(report->expected) << '\n';
+			std::cout << "Observed:\t" << *reinterpret_cast<uint32_t*>(report->observed) << '\n';
+			break;
+		case badhron::CheckType::UInt16:
+			std::cout << "Expected:\t" << *reinterpret_cast<uint16_t*>(report->expected) << '\n';
+			std::cout << "Observed:\t" << *reinterpret_cast<uint16_t*>(report->observed) << '\n';
+			break;
+		case badhron::CheckType::UInt8:
+			std::cout << "Expected:\t" << int(*reinterpret_cast<uint8_t*>(report->expected)) << '\n';
+			std::cout << "Observed:\t" << int(*reinterpret_cast<uint8_t*>(report->observed)) << '\n';
+			break;
+		case badhron::CheckType::Float64:
+			std::cout << "Expected:\t" << *reinterpret_cast<double*>(report->expected) << '\n';
+			std::cout << "Observed:\t" << *reinterpret_cast<double*>(report->observed) << '\n';
+			break;
+		case badhron::CheckType::Float32:
+			std::cout << "Expected:\t" << *reinterpret_cast<float*>(report->expected) << '\n';
+			std::cout << "Observed:\t" << *reinterpret_cast<float*>(report->observed) << '\n';
 			break;
 		default:
 			break;
