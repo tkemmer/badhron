548a007ca6f0c681b0e940bed2d70bdf9250eb1d
Thomas Kemmer <thomas@binaryden.de>
2018-10-01 15:11:16 +0200

    Use template class for check reports in C++

--- src/reports.cpp
+++ src/reports.cpp
@@ -1,70 +1,42 @@
 #include "reports.h"
-#include <iostream>
 #include <iomanip>
 
-using namespace std::string_literals;
-
-namespace badhron {
-
-	CheckReport::CheckReport(const Prototype& proto) :
-		function_{proto.function},
-		subgroup_{proto.subgroup ? proto.subgroup : ""},
-		message_{proto.message} {
-	}
-
-}
-
-void badhron_print_report(badhron::CheckReport::Prototype* report) {
-	std::cout << " # "s << report->function;
-	if(report->subgroup)
-		std::cout << " (in subgroup "s << report->subgroup << ")"s;
-	std::cout << '\n'
-	          << "   \e[1m"s << report->message << "\e[0m\n"s;
-	switch(report->ctype) {
+void badhron_print_report(badhron::CheckReportPrototype* proto) {
+	using badhron::CheckReport;
+	std::cout << std::boolalpha;
+	switch(proto->ctype) {
 		case badhron::CheckType::Bool:
-			std::cout << std::boolalpha;
-			std::cout << "   Expected: "s << *reinterpret_cast<bool*>(report->expected) << '\n';
-			std::cout << "   Observed: "s << *reinterpret_cast<bool*>(report->observed) << '\n';
+			std::cout << CheckReport<bool>(*proto);
 			break;
 		case badhron::CheckType::Int64:
-			std::cout << "   Expected: "s << *reinterpret_cast<int64_t*>(report->expected) << '\n';
-			std::cout << "   Observed: "s << *reinterpret_cast<int64_t*>(report->observed) << '\n';
+			std::cout << CheckReport<int64_t>(*proto);
 			break;
 		case badhron::CheckType::Int32:
-			std::cout << "   Expected: "s << *reinterpret_cast<int32_t*>(report->expected) << '\n';
-			std::cout << "   Observed: "s << *reinterpret_cast<int32_t*>(report->observed) << '\n';
+			std::cout << CheckReport<int32_t>(*proto);
 			break;
 		case badhron::CheckType::Int16:
-			std::cout << "   Expected: "s << *reinterpret_cast<int16_t*>(report->expected) << '\n';
-			std::cout << "   Observed: "s << *reinterpret_cast<int16_t*>(report->observed) << '\n';
+			std::cout << CheckReport<int16_t>(*proto);
 			break;
 		case badhron::CheckType::Int8:
-			std::cout << "   Expected: "s << int(*report->expected) << '\n';
-			std::cout << "   Observed: "s << int(*report->observed) << '\n';
+			std::cout << CheckReport<int8_t>(*proto);
 			break;
 		case badhron::CheckType::UInt64:
-			std::cout << "   Expected: "s << *reinterpret_cast<uint64_t*>(report->expected) << '\n';
-			std::cout << "   Observed: "s << *reinterpret_cast<uint64_t*>(report->observed) << '\n';
+			std::cout << CheckReport<uint64_t>(*proto);
 			break;
 		case badhron::CheckType::UInt32:
-			std::cout << "   Expected: "s << *reinterpret_cast<uint32_t*>(report->expected) << '\n';
-			std::cout << "   Observed: "s << *reinterpret_cast<uint32_t*>(report->observed) << '\n';
+			std::cout << CheckReport<uint32_t>(*proto);
 			break;
 		case badhron::CheckType::UInt16:
-			std::cout << "   Expected: "s << *reinterpret_cast<uint16_t*>(report->expected) << '\n';
-			std::cout << "   Observed: "s << *reinterpret_cast<uint16_t*>(report->observed) << '\n';
+			std::cout << CheckReport<uint16_t>(*proto);
 			break;
 		case badhron::CheckType::UInt8:
-			std::cout << "   Expected: "s << int(*reinterpret_cast<uint8_t*>(report->expected)) << '\n';
-			std::cout << "   Observed: "s << int(*reinterpret_cast<uint8_t*>(report->observed)) << '\n';
+			std::cout << CheckReport<uint8_t>(*proto);
 			break;
 		case badhron::CheckType::Float64:
-			std::cout << "   Expected: "s << *reinterpret_cast<double*>(report->expected) << '\n';
-			std::cout << "   Observed: "s << *reinterpret_cast<double*>(report->observed) << '\n';
+			std::cout << CheckReport<double>(*proto);
 			break;
 		case badhron::CheckType::Float32:
-			std::cout << "   Expected: "s << *reinterpret_cast<float*>(report->expected) << '\n';
-			std::cout << "   Observed: "s << *reinterpret_cast<float*>(report->observed) << '\n';
+			std::cout << CheckReport<float>(*proto);
 			break;
 		default:
 			break;
--- src/reports.h
+++ src/reports.h
@@ -2,6 +2,7 @@
 #define BADHRON_REPORTS_H
 
 #include <cstdint>
+#include <iostream>
 #include <string>
 
 namespace badhron {
@@ -13,28 +14,49 @@ namespace badhron {
 		Float64, Float32, Float16
 	};
 
+	struct CheckReportPrototype {
+		const char* function;
+		const char* subgroup;
+		const char* message;
+		CheckType   ctype;
+		int8_t*     expected;
+		int8_t*     observed;
+	};
+
+	template<class T>
 	class CheckReport {
 	public:
-		struct Prototype {
-			const char* function;
-			const char* subgroup;
-			const char* message;
-			CheckType   ctype;
-			int8_t*     expected;
-			int8_t*     observed;
-		};
+		explicit CheckReport(const CheckReportPrototype& proto) :
+			function_{proto.function},
+			subgroup_{proto.subgroup ? proto.subgroup : ""},
+			message_{proto.message},
+			expected_{*reinterpret_cast<T*>(proto.expected)},
+			observed_{*reinterpret_cast<T*>(proto.observed)} {
+		}
 
-		explicit CheckReport(const Prototype& proto);
+		friend std::ostream& operator<<(std::ostream& os, const CheckReport<T>& report) {
+			using namespace std::string_literals;
+			os << " # "s << report.function_;
+			if(report.subgroup_ != ""s)
+				os << " (in subgroup "s << report.subgroup_ << ")"s;
+			os << '\n'
+			   << "   \e[1m"s << report.message_ << "\e[0m\n"s
+			   << "   Expected: "s << report.expected_ << '\n'
+			   << "   Observed: "s << report.observed_ << '\n';
+			return os;
+		}
 
 	private:
 		std::string function_;
 		std::string subgroup_;
 		std::string message_;
+		T expected_;
+		T observed_;
 	};
 }
 
 extern "C" {
-	void badhron_print_report(badhron::CheckReport::Prototype *report);
+	void badhron_print_report(badhron::CheckReportPrototype *report);
 }
 
 #endif // BADHRON_REPORTS_H
