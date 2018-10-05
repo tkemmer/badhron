e918cff956827c80d509b2fd1bbeb2e6757bdbb3
Thomas Kemmer <thomas@binaryden.de>
2018-10-05 15:30:47 +0200

    Rename {result_handler=>suite}

--- src/cpp/suite.cpp
+++ src/cpp/suite.cpp
@@ -12,81 +12,81 @@ namespace badhron {
 	using std::ostream;
 	using std::string;
 
-	static uint64_t last_handler_id = 0;
-	static std::map<uint64_t, result_handler> handlers;
+	static uint64_t last_suite_id = 0;
+	static std::map<uint64_t, suite> suites;
 
-	void result_handler::add_group(string name) {
+	void suite::add_group(string name) {
 		groups_.emplace_back(move(name));
 	}
 
-	void result_handler::add_subgroup(std::string name) {
+	void suite::add_subgroup(std::string name) {
 		if(!groups_.empty()) // TODO else
 			groups_.back().add_subgroup(move(name));
 	}
 
-	void result_handler::add_report(report report) {
+	void suite::add_report(report report) {
 		if(!groups_.empty()) // TODO else
 			groups_.back().add_report(move(report));
 	}
 
-	void result_handler::group_result(result result) {
+	void suite::group_result(result result) {
 		if(!groups_.empty()) // TODO else
 			groups_.back().group_result(result);
 	}
 
-	void result_handler::subgroup_result(result result) {
+	void suite::subgroup_result(result result) {
 		if(!groups_.empty()) // TODO else
 			groups_.back().subgroup_result(result);
 	}
 
-	ostream& operator<<(ostream& os, const result_handler& handler) {
-		for(auto& group: handler.groups_)
+	ostream& operator<<(ostream& os, const suite& suite) {
+		for(auto& group: suite.groups_)
 			std::cout << group;
 		return os;
 	}
 }
 
-uint64_t badhron_create_result_handler() {
-	badhron::handlers[badhron::last_handler_id];
-	return badhron::last_handler_id++;
+uint64_t badhron_create_suite() {
+	badhron::suites[badhron::last_suite_id];
+	return badhron::last_suite_id++;
 }
 
-void badhron_delete_result_handler(uint64_t which) {
-	badhron::handlers.erase(which);
+void badhron_delete_suite(uint64_t which) {
+	badhron::suites.erase(which);
 }
 
-void badhron_add_group(uint64_t handler, char* name) {
-	badhron::handlers[handler].add_group(name);
+void badhron_add_group(uint64_t suite, char* name) {
+	badhron::suites[suite].add_group(name);
 }
 
-void badhron_add_subgroup(uint64_t handler, char* name) {
-	badhron::handlers[handler].add_subgroup(name);
+void badhron_add_subgroup(uint64_t suite, char* name) {
+	badhron::suites[suite].add_subgroup(name);
 }
 
-void badhron_set_group_result(uint64_t handler, int32_t passed, int32_t failed, int32_t pending) {
-	badhron::handlers[handler].group_result(badhron::result{passed, failed, pending});
+void badhron_set_group_result(uint64_t suite, int32_t passed, int32_t failed, int32_t pending) {
+	badhron::suites[suite].group_result(badhron::result{passed, failed, pending});
 }
 
-void badhron_set_subgroup_result(uint64_t handler, int32_t passed, int32_t failed, int32_t pending) {
-	badhron::handlers[handler].subgroup_result(badhron::result{passed, failed, pending});
+void badhron_set_subgroup_result(uint64_t suite, int32_t passed, int32_t failed, int32_t pending) {
+	badhron::suites[suite].subgroup_result(badhron::result{passed, failed, pending});
 }
 
-void badhron_print_suite(uint64_t handler) {
-	std::cout << badhron::handlers[handler];
+void badhron_print_suite(uint64_t suite) {
+	std::cout << badhron::suites[suite];
 }
 
-void badhron_add_bool_report(uint64_t handler, char* fn, char* subgrp, char* msg, bool exp, bool obs) {
-	badhron::handlers[handler].add_report(badhron::report{fn, subgrp, msg, exp, obs});
+void badhron_add_bool_report(uint64_t suite, char* fn, char* subgrp, char* msg, bool exp, bool obs) {
+	badhron::suites[suite].add_report(badhron::report{fn, subgrp, msg, exp, obs});
 }
 
-void badhron_add_int_report(uint64_t handler, char* fn, char* subgrp, char* msg, int64_t exp, int64_t obs) {
-	badhron::handlers[handler].add_report(badhron::report{fn, subgrp, msg, exp, obs});
+void badhron_add_int_report(uint64_t suite, char* fn, char* subgrp, char* msg, int64_t exp, int64_t obs) {
+	badhron::suites[suite].add_report(badhron::report{fn, subgrp, msg, exp, obs});
 }
 
-void badhron_add_uint_report(uint64_t handler, char* fn, char* subgrp, char* msg, uint64_t exp, uint64_t obs) {
-	badhron::handlers[handler].add_report(badhron::report{fn, subgrp, msg, exp, obs});
+void badhron_add_uint_report(uint64_t suite, char* fn, char* subgrp, char* msg, uint64_t exp, uint64_t obs) {
+	badhron::suites[suite].add_report(badhron::report{fn, subgrp, msg, exp, obs});
 }
 
-void badhron_add_float_report(uint64_t handler, char* fn, char* subgrp, char* msg, double exp, double obs) {
-	badhron::handlers[handler].add_report(badhron::report{fn, subgrp, msg, exp, obs});
+void badhron_add_float_report(uint64_t suite, char* fn, char* subgrp, char* msg, double exp, double obs) {
+	badhron::suites[suite].add_report(badhron::report{fn, subgrp, msg, exp, obs});
 }
--- src/cpp/suite.h
+++ src/cpp/suite.h
@@ -10,8 +10,7 @@ namespace badhron {
 	class report;
 	class result;
 
-	// TODO rename (suite)
-	class result_handler {
+	class suite {
 	public:
 		void add_group(std::string name);
 		void add_subgroup(std::string name);
@@ -20,7 +19,7 @@ namespace badhron {
 		void group_result(result);
 		void subgroup_result(result);
 
-		friend std::ostream& operator<<(std::ostream& os, const result_handler& handler);
+		friend std::ostream& operator<<(std::ostream& os, const suite& suite);
 
 	private:
 		std::vector<group> groups_;
@@ -29,21 +28,21 @@ namespace badhron {
 }
 
 extern "C" {
-uint64_t badhron_create_result_handler();
-void badhron_delete_result_handler(uint64_t which);
+	uint64_t badhron_create_suite();
+	void badhron_delete_suite(uint64_t which);
 
-void badhron_add_group(uint64_t handler, char* name);
-void badhron_add_subgroup(uint64_t handler, char* name);
+	void badhron_add_group(uint64_t suite, char* name);
+	void badhron_add_subgroup(uint64_t suite, char* name);
 
-void badhron_set_group_result(uint64_t handler, int32_t passed, int32_t failed, int32_t pending);
-void badhron_set_subgroup_result(uint64_t handler, int32_t passed, int32_t failed, int32_t pending);
+	void badhron_set_group_result(uint64_t suite, int32_t passed, int32_t failed, int32_t pending);
+	void badhron_set_subgroup_result(uint64_t suite, int32_t passed, int32_t failed, int32_t pending);
 
-void badhron_print_suite(uint64_t handler);
+	void badhron_print_suite(uint64_t suite);
 
-void badhron_add_bool_report( uint64_t handler, char* fn, char* subgrp, char* msg, bool     exp, bool     obs);
-void badhron_add_int_report(  uint64_t handler, char* fn, char* subgrp, char* msg, int64_t  exp, int64_t  obs);
-void badhron_add_uint_report( uint64_t handler, char* fn, char* subgrp, char* msg, uint64_t exp, uint64_t obs);
-void badhron_add_float_report(uint64_t handler, char* fn, char* subgrp, char* msg, double   exp, double   obs);
+	void badhron_add_bool_report( uint64_t suite, char* fn, char* subgrp, char* msg, bool     exp, bool     obs);
+	void badhron_add_int_report(  uint64_t suite, char* fn, char* subgrp, char* msg, int64_t  exp, int64_t  obs);
+	void badhron_add_uint_report( uint64_t suite, char* fn, char* subgrp, char* msg, uint64_t exp, uint64_t obs);
+	void badhron_add_float_report(uint64_t suite, char* fn, char* subgrp, char* msg, double   exp, double   obs);
 }
 
 #endif // BADHRON_SUITE_H
